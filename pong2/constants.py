5c2797327c5fe85e16356a086d9cbfb6b9e31a7e
blinkythefish <pottle.jamin@gmail.com>
2022-12-07 19:21:17 -0800

    started spliting code into classes

---
+++ pong2/constants.py
@@ -0,0 +1,16 @@
+import pygame
+
+
+
+WINNING_SCORE = 10
+
+FPS = 60
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
+BALL_RADIUS = 7
+
+
+WIDTH, HEIGHT = 700, 500
