fb6ec77217f75113d02278eec07e25559c8f682b
Daniel Alvarez <104532965+Daniel-Alvarez-BYUI@users.noreply.github.com>
2022-12-08 21:12:58 -0400

    Update ball.py

--- pong/cast/ball.py
+++ pong/cast/ball.py
@@ -2,9 +2,19 @@ from constants import *
 import pygame
 
 class Ball:
+    """
+    what we want from the ball is  x , y and the radius
+    x = x-coordinate
+    y = y-coordinate
+    radius = radius of the ball
+    x_vel = volocity in X
+    y_vel = velocity in Y
+    """
+    
     MAX_VEL = 5
 
     def __init__(self, x, y, radius, color):
+        """Constructs a new ball."""
         self.x = self.original_x = x
         self.y = self.original_y = y
         self.radius = radius
@@ -13,9 +23,15 @@ class Ball:
         self.color = color
 
     def draw(self, win):
+         """Gets the window.
+        
+        Returns:
+            help to draw the ball
+        """
         pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
 
     def move(self):
+        """help in the movement of the ball"""
         self.x += self.x_vel
         self.y += self.y_vel
 
