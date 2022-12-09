c6643149b7d82c99b8526581c47f5c95ea5c35bd
Webdeveloperintraining <sil22006@byui.edu>
2022-12-08 17:03:14 -0800

    comments 80% percent complete

--- pong/physics/movement.py
+++ pong/physics/movement.py
@@ -1,15 +1,9 @@
-'''
-
-default class for movement in pong
-
-'''
-
-
-
 class Movement:
-    
-    
+    '''
+    default class for movement in pong
+    '''
     def execute():
-        
-        
+        '''
+        This method purpose is to be overriden by collision and paddles classes
+        '''
         raise NotImplementedError("execute not implemented in base class")
--- pong/physics/paddles.py
+++ pong/physics/paddles.py
@@ -1,17 +1,21 @@
-
 import pygame
 from physics.movement import Movement
 from constants import *
 
-
 class PaddleMovement(Movement):
-    
-    
-    def __init__(self) -> None:
+    '''
+    Class in charge of making the paddles move and doing it at the right speed when a key is pressed.
+    '''
+    def __init__(self):
         super().__init__()
-        
-        
+             
     def execute(self, keys, left_paddle, right_paddle):
+        '''
+        ARGS:
+            keys: calling pygame module code to make that the paddles move with certain keys
+            left_paddle: First player paddle
+            right_paddle: Second player paddle
+        '''
         if keys[pygame.K_w] and left_paddle.y - left_paddle.VEL >= 0:
             left_paddle.move(up=True)
         if keys[pygame.K_s] and left_paddle.y + left_paddle.VEL + left_paddle.height <= HEIGHT:
